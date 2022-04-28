17f7c30bb7493165b748347941fecb8a0ab0fe91
JonathanLaureano <JonathanLaureano20@gmail.com>
2022-04-28 11:53:47 -0400

    integerReversal

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,18 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let result = ''
+    let sign = Math.sign(n)
+    let num = Math.abs(n)
+    let numStr = num.toString()
+    if (n === 0){
+        return 0
+    } else {
+        for (let i = numStr.length-1; i >= 0; i--){ 
+            result += numStr[i]
+    }}
+    return parseInt(result) * sign
+}
 
 module.exports = reverseInt;

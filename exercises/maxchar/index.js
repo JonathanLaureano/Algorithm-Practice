c41227958a2af3ae76b413e42acb290844700858
JonathanLaureano <JonathanLaureano20@gmail.com>
2022-04-28 16:05:25 -0400

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let chars = {}
+    let max = 0
+    let maxChar = ''
+    for (let char of str) {
+        chars[char] = chars[char] + 1 || 1;
+    }
+    for (let i in chars) {
+        if (chars[i] > max){
+            max = chars[i]
+            maxChar = i
+        }
+    }
+    return maxChar
+}
 
 module.exports = maxChar;
